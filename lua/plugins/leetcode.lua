c96e2944f0d128ce302bb5319c5268b9b738da19
l4m4r <dev@vophan.day>
2024-02-02 23:57:39 +0800

    feat: add leetcode enhanced config

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -1,21 +1,26 @@
 return {
-    "kawre/leetcode.nvim",
-    build = ":TSUpdate html",
-    dependencies = {
-        "nvim-telescope/telescope.nvim",
-        "nvim-lua/plenary.nvim", -- required by telescope
-        "MunifTanjim/nui.nvim",
+	"kawre/leetcode.nvim",
+	build = ":TSUpdate html",
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+		"nvim-lua/plenary.nvim", -- required by telescope
+		"MunifTanjim/nui.nvim",
 
-        -- optional
-        "nvim-treesitter/nvim-treesitter",
-        "rcarriga/nvim-notify",
-        "nvim-tree/nvim-web-devicons",
-    },
-    opts = {
-        lang = "golang",
-        cn = {
-            enabled = true,
-        },
-    },
-    cmd = { "Leet" },
+		-- optional
+		"nvim-treesitter/nvim-treesitter",
+		"rcarriga/nvim-notify",
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("leetcode").setup({
+			lang = "golang",
+			cn = {
+				enabled = true,
+			},
+		})
+
+		local leetcode_enhanced = require("custom.leetcode")
+		leetcode_enhanced.setup()
+	end,
+	cmd = { "Leet", "LeetToday" },
 }
