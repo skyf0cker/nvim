c536fe297532e2f6ef5427a0e351b0f4f2a78d0e
l4m4r <dev@vophan.day>
2024-02-21 12:00:56 +0800

    feat: add status line

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -2,6 +2,11 @@ return {
 	"echasnovski/mini.hipatterns",
 	version = "*",
 	config = function()
+		vim.api.nvim_set_hl(0, "MiniHiPatternsLogInfo", { fg = "#ffffff", bg = "#73e861", bold = true })
+		vim.api.nvim_set_hl(0, "MiniHiPatternsLogError", { fg = "#ffffff", bg = "#ed495f", bold = true })
+		vim.api.nvim_set_hl(0, "MiniHiPatternsLogWarn", { fg = "#ffffff", bg = "#f0f25a", bold = true })
+		vim.api.nvim_set_hl(0, "MiniHiPatternsLogDebug", { fg = "#ffffff", bg = "#0000ff", bold = true })
+
 		local hipatterns = require("mini.hipatterns")
 		hipatterns.setup({
 			highlighters = {
@@ -10,6 +15,11 @@ return {
 				hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
 				todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
 				note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+				info = { pattern = "%f[%w]()INFO()%f[%W]", group = "MiniHipatternsLogInfo" },
+				error = { pattern = "%f[%w]()ERROR()%f[%W]", group = "MiniHipatternsLogError" },
+				warn = { pattern = "%f[%w]()WARN()%f[%W]", group = "MiniHipatternsLogWarn" },
+				debug = { pattern = "%f[%w]()DEBUG()%f[%W]", group = "MiniHipatternsLogDebug" },
 
 				-- Highlight hex color strings (`#rrggbb`) using that color
 				hex_color = hipatterns.gen_highlighter.hex_color(),
