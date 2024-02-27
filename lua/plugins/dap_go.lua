0ae48cf1d66308fc43017bbde9c6496b841d7e5b
l4m4r <dev@vophan.day>
2024-02-27 19:50:03 +0800

    feat: dap-go add remote attacgh

--- lua/plugins/dap_go.lua
+++ lua/plugins/dap_go.lua
@@ -4,6 +4,23 @@ return {
 		"mfussenegger/nvim-dap",
 	},
 	config = function()
-		require("dap-go").setup()
+		require("dap-go").setup({
+			dap_configurations = {
+				{
+					type = "go",
+					name = "Attach remote",
+					mode = "remote",
+					request = "attach",
+					-- tell which host and port to connect to
+					connect = {
+						host = "127.0.0.1",
+						port = "8181",
+					},
+				},
+			},
+			delve = {
+				port = "8181",
+			},
+		})
 	end,
 }
