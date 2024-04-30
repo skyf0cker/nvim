27fdcdde058ec29cc39e2b7d33daf72762e18fe9
l4m4r <dev@vophan.day>
2024-04-30 14:21:14 +0800

    feat: using neogit instead of lazygit

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,33 +1,53 @@
 return {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-        require("gitsigns").setup({
-            signcolumn = true,
-        })
-    end,
-    lazy = true,
-    keys = {
-        {
-            "<leader>gb",
-            function()
-                vim.cmd([[Gitsigns toggle_current_line_blame]])
-            end,
-            desc = "[G]it [B]lame",
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup({
+                signcolumn = true,
+            })
+        end,
+        lazy = true,
+        keys = {
+            {
+                "<leader>gb",
+                function()
+                    vim.cmd([[Gitsigns toggle_current_line_blame]])
+                end,
+                desc = "[G]it [B]lame",
+            },
+            {
+                "<leader>gp",
+                function()
+                    vim.cmd([[Gitsigns preview_hunk]])
+                end,
+                desc = "[G]it [P]review hunk",
+            },
+            {
+                "<leader>gd",
+                function()
+                    vim.cmd([[Gitsigns diffthis]])
+                end,
+                desc = "[G]it [D]iff",
+            },
         },
-        {
-            "<leader>gp",
-            function()
-                vim.cmd([[Gitsigns preview_hunk]])
-            end,
-            desc = "[G]it [P]review hunk",
+        event = { "BufReadPost", "BufNewFile" },
+    },
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim", -- required
+            "sindrets/diffview.nvim", -- optional - Diff integration
+            "ibhagwan/fzf-lua", -- optional
         },
-        {
-            "<leader>gd",
-            function()
-                vim.cmd([[Gitsigns diffthis]])
-            end,
-            desc = "[G]it [D]iff",
+        config = true,
+        keys = {
+            {
+                "<leader>gt",
+                function()
+                    require("neogit").open()
+                end,
+                desc = "[G]it [T]oggle",
+            },
         },
     },
-    event = { "BufReadPost", "BufNewFile" },
 }
