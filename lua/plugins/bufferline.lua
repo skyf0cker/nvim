49029e3f8a3610ca26c72d4497a533acdb78c264
patricklamar <dev@vophan.day>
2024-05-03 13:42:48 +0800

    update

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -12,7 +12,6 @@ return {
                     local icon = level:match("error") and " " or " "
                     return " " .. icon .. count
                 end,
-                separator_style = "slant",
                 show_close_icon = false,
                 show_buffer_close_icons = false,
                 offsets = {
