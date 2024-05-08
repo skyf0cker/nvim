c41f85b1f28be181d3442cc19df0bf1cf73d964d
l4m4r <dev@vophan.day>
2024-05-08 10:54:44 +0800

    update

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,10 +1,9 @@
 return {
-    "stevearc/oil.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        require("oil").setup()
-        vim.keymap.set("n", "<C-b>", function()
-            require("oil").toggle_float()
-        end, {})
-    end,
+	"stevearc/oil.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("oil").setup()
+
+		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+	end,
 }
